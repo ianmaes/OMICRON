aa7384dfc735ead58de573467e3834e0a0edbe67
ArthurVDS07 <101700825+ArthurVDS07@users.noreply.github.com>
2024-01-11 10:13:31 +0100

    cleanup

--- notebooks/Train.py
+++
@@ -1,8 +0,0 @@
-import os
-import torch
-import torch.nn as nn 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  
-os.environ["CUDA_VISIBLE_DEVICES"]="0" # GPU index
-
-device = torch.device('cuda:0') if torch.cuda.is_available() else 'cpu'
-print(device)
